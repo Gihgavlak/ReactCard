031e834205a125aa7b62d5ffae15ad84e590cce4
Gislaine Maria Gavlak <gislaine.gavlak@zup.com.br>
2022-01-13 15:26:44 -0300

    feat: Criar componente para layout

---
+++ src/components/layout/Card.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default props =>
+    <div className="Card">
+        <div className="Footer">
+            {props.titulo}
+
+        </div>
+
+    </div>
--- src/index.js
+++ src/index.js
@@ -4,17 +4,21 @@ import './index.css'
 //import Primeiro from './components/Primeiro';
 //import ComParametro from './components/ComParametro'
 import ComFilhos from './components/ComFilhos'
+import Card from './components/layout/Card'
 
 ReactDOM.render(
     <div>
-        <ComFilhos>
+        <Card titulo="Exercício X">
+            Conteudo
+        </Card>
+        {/*<ComFilhos>
             <ul>
                <li>Ana</li>
                <li>Bia</li>
                <li>Carlos</li>
                <li>Daniel</li>
             </ul>
-        </ComFilhos>
+        </ComFilhos>*/}
 
         {/*<Primeiro/>
         <ComParametro titulo="Esse é o título"
