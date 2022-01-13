bff23f72b5ac2a4e2f68b574450b10efa956a1ef
Gislaine Maria Gavlak <gislaine.gavlak@zup.com.br>
2022-01-13 15:11:52 -0300

    feat: Iportar e passar os elrmentos

--- src/index.js
+++ src/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css'
-import Primeiro from './components/Primeiro';
-import ComParametro from './components/ComParametro'
+//import Primeiro from './components/Primeiro';
+//import ComParametro from './components/ComParametro'
+import ComFilhos from './components/ComFilhos'
 
 ReactDOM.render(
     <div>
-         <Primeiro/>
+        <ComFilhos>
+            <ul>
+               <li>Ana</li>
+               <li>Bia</li>
+               <li>Carlos</li>
+               <li>Daniel</li>
+            </ul>
+        </ComFilhos>
+
+        {/*<Primeiro/>
         <ComParametro titulo="Esse é o título"
-        subtitulo="Esse é o subtitulo"/>
+        subtitulo="Esse é o subtitulo"/>*/}
     </div>,
    
     document.getElementById('root')
