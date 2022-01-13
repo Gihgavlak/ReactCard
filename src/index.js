e4dfbb0faab07a8a6f80a6a5e362da3c53b7ecc3
Gislaine Maria Gavlak <gislaine.gavlak@zup.com.br>
2022-01-13 14:56:54 -0300

    feat: Criar novo componente e instanciar

--- src/index.js
+++ src/index.js
@@ -2,9 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css'
 import Primeiro from './components/Primeiro';
+import ComParametro from './components/ComParametro'
 
 ReactDOM.render(
-    <Primeiro/>,
+    <div>
+         <Primeiro/>
+        <ComParametro titulo="Esse é o título"
+        subtitulo="Esse é o subtitulo"/>
+    </div>,
+   
     document.getElementById('root')
 
 )
