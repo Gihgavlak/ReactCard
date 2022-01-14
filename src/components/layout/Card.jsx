a5371f84ee3d65bdaadf8b98d2704518e6705443
Gislaine Maria Gavlak <gislaine.gavlak@zup.com.br>
2022-01-14 11:25:24 -0300

    feat: Exemplo de comunicação direta, pai e filho

--- src/components/layout/Card.jsx
+++ src/components/layout/Card.jsx
@@ -3,11 +3,15 @@ import './Card.css'
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default props =>
-    <div className="Card">
+    <div className="Card" style={{
+        borderColor: props.cplor || '#000'
+        }}>
         <div className="Conteudo">
             {props.children}
         </div>
-        <div className="Footer">
+        <div className="Footer" style={{
+            backgroundColor: props.cplor || '#000'
+            }}>
             {props.titulo}
         </div>
     </div>
